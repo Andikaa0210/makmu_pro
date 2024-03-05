be5140224bab4300eaa7d27911b7101e0ecb4681
unknown <andika23115651@students.amikom.ac.id>
2024-03-05 08:18:29 +0700

    first commit

---
+++ pertemuan1tugascalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan1tugascalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+
+            Console.Title = "Aplikasi calculator";
+
+            int a = 10; // declarasi variable a, dg nilai awal 10;
+            int b = 6; // declarasi variable b, dg nilai awal 6;
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+        static void Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        
+    }
+}
